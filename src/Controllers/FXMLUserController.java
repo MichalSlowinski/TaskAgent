784304090d9bd020e830553e47d6805e353a04d5
Skitels1 <slowinskimichal93@gmail.com>
2016-04-21 19:09:40 +0200

    New Commit

--- src/Controllers/FXMLUserController.java
+++ src/Controllers/FXMLUserController.java
@@ -3,6 +3,8 @@ package Controllers;
 import Logic.WindowsOpener;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,26 +13,41 @@ import javafx.scene.control.ListView;
 public class FXMLUserController {
 
     @FXML
+    
     private ResourceBundle resources;
 
     @FXML
+    
     private URL location;
 
     @FXML
-    private ListView<?> StartedTask;
-
+    
+    private ListView StartedTask;
+    
     @FXML
+    
     private Button StartTask;
 
     @FXML
+    
+    private Button LoadTasks;
+     
+    @FXML
+    
     private Button Logout;
 
     @FXML
+    
     private Button Finish;
 
-    @FXML
-    private ListView<?> WaitingTask;
+    
 
+    public static final ObservableList WaitingTasks = 
+        FXCollections.observableArrayList();
+
+    public static final ObservableList StartedTasks = 
+        FXCollections.observableArrayList();
+    
     @FXML
     void HandleStartTaskButtonAction(ActionEvent event) {
 
@@ -45,8 +62,12 @@ public class FXMLUserController {
     void HandleLogoutButtonAction(ActionEvent event) {
      WindowsOpener.logout();
     }
-
     @FXML
+    void HandleLoadTasksButtonAction(ActionEvent event) {
+        WaitingTasks.add("coś");
+        ListView Waitinglist = new ListView();
+    Waitinglist.setItems(WaitingTasks);
+    }
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
